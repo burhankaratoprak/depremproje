fe2de1f21002bfa144aae32dae6bcdddb2ca4692
burhankaratoprak <118635013+burhankaratoprak@users.noreply.github.com>
2023-05-17 02:37:08 +0300

    proje.py

--- Erzak kontrol ve yardım/proje.py
+++ Erzak kontrol ve yardım/proje.py
@@ -1,3 +1,8 @@
+kullanicilar = [
+    {"kullanici_adi": "admin", "sifre": "admin", "yetki": "admin"},
+    {"kullanici_adi": "görevli1", "sifre": "123456", "yetki": "Görevli1"},
+]
+
 while True:
     print("1.Görevli Girişi")
     print("2.Depremzedeler İçin Yardım Talebi")
@@ -6,8 +11,7 @@ while True:
     secim=int(input("Lütfen bir işlem seçiniz: "))
     
     if secim==1:
-        dogru_bilgi=True
-        if dogru_bilgi:
+        
          while True:
              print("1.Sisteme Üye Ol")
              print("2.Sisteme Giriş Yap")
@@ -22,23 +26,47 @@ while True:
                  sifre = input("Şifrenizi girin: ")
                  sifre2=input("Şifrenizi tekrar girin:")
                  if sifre==sifre2:
-                     print("Üyeliğiniz Başarıyla oluşturulmuştur.")
-                     
+                   kullanicilar.append({"kullanici_adi": kullanici_adi, "sifre": sifre, "yetki": "Görevli1"})
+                   print("Üyeliğiniz Başarıyla oluşturulmuştur.")
                  else:
                      print("Şifreler eşleşmemektedir.Tekrar deneyiniz")
                      continue
              elif secim == 2:
                  kullanici_adi = input("Kullanıcı adınızı girin: ")
                  sifre = input("Şifrenizi girin: ")
-                 if dogru_bilgi:
-                     print("giriş başarılı")
-                     # Veritabanından kullanıcı bilgileri kontrol edilir
+                 for kullanici in kullanicilar:
+                  if kullanici["kullanici_adi"] == kullanici_adi and kullanici["sifre"] == sifre:
+                   if kullanici["yetki"] == "admin":
+                     print("Admin girişi başarılı.")
+                     while True:
+                         print("1. Depremzedeleri Listele")
+                         print("2. Depremzedelere Yardım  Gönder")
+                         print("3. Çıkış")
+                         secim=int(input("Lütfen bir işlem seçiniz:"))
+                         if secim==1:
+                             print("Depremzedeler Listeleniyor...")
+                         elif secim == 2:
+                                  print("Depremzedelere yardım gönderiliyor...")
+                                  # Yardım talepleri kabul edilip depremzedelere yardım gönderilir.
+                         elif secim == 3:
+                                  print("Çıkış yapılıyor...")
+                                  break
+                         else:
+                            print("Geçersiz işlem seçimi. Lütfen tekrar deneyin.")
+                  elif kullanici["yetki"] == "Görevli1":
+                   print("Kullanıcı girişi başarılı.")
+                   # Kullanıcıya özel işlemler yapılabilir
+                  break
+                 else:
+                  print("Hatalı kullanıcı adı veya şifre. Lütfen tekrar deneyin.")
              elif secim == 3:
                  kullanici_adi = input("Kullanıcı adınızı girin: ")
+                 for kullanici in kullanicilar:
+                     if kullanici["kullanici_adi"] == kullanici_adi:
                      # Veritabanından kullanıcı bilgileri kontrol edilir
-                 if dogru_bilgi:
-                     print("yeni şifreniz 123")
-                     # Kullanıcının şifresi yenilenebilir
+                      print("yeni şifreniz 123")
+                      break
+                      # Kullanıcının şifresi yenilenebilir
              elif secim == 4:
                  break
              else:
